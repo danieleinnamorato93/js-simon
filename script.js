5165d044de565134b464695378fed7651524eab4
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-27 16:58:38 +0200

    fnz startgame

--- script.js
+++ script.js
@@ -22,3 +22,58 @@ function generateRandomNumbers() {
     }
     return numbers;
 }
+
+function showNumbers() {
+    numbersContainer.innerText = randomNumbers.join(' '); 
+}
+
+//creo una funzione per nascondere e mostrare input
+
+function showInputs() {
+    numbersContainer.style.display = 'none'; 
+    inputsContainer.innerHTML = ''; 
+
+    // creo 5 campi di input
+    for (let i = 0; i < numbersCount; i++) {
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'number-input'; 
+        inputsContainer.appendChild(input);
+    }
+
+    checkButton.style.display = 'block'; 
+}
+
+// Funzione di verifica dei numeri inseriti dall'utente
+function checkNumbers() {
+    let userInputs = document.querySelectorAll('.number-input');
+    let userNumbers = [];
+    
+    for (let i = 0; i < userInputs.length; i++) {
+        let value = userInputs[i].value;
+        if (value !== '' && !isNaN(value)) {
+            userNumbers.push(parseInt(value));
+        }
+    }
+
+    let correctNumbers = 0;
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (userNumbers.includes(randomNumbers[i])) {
+            correctNumbers++; 
+        }
+    }
+
+    resultParagraph.innerText = 'Hai indovinato ' + correctNumbers + ' numero/i!';
+}
+
+//funzione per inizializzare il gioco
+
+function startGame() {
+    randomNumbers = generateRandomNumbers(); 
+    showNumbers(); 
+
+    setTimeout(function() {
+        showInputs(); 
+    }, displayTime);
+}
+
